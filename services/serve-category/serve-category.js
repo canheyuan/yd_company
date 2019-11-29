3e416f2f91d952d0875e6a8e35b2a2c71c21464b
canheyuan <429501203@qq.com>
2019-11-29 14:10:56 +0800

    1.4.2发布备份

--- services/serve-category/serve-category.js
+++ services/serve-category/serve-category.js
@@ -1,13 +1,12 @@
 //获取应用实例
 const app = getApp()
+
 Page({
     //页面的初始数据
     data: {
         domainUrl: app.globalData.domainUrl,
-
         serveData:{},
         levelId:'',
-
         langData: null,  //语言数据
         lang: '',    //语言类型
     },
@@ -19,18 +18,21 @@ Page({
             wx.setNavigationBarTitle({ title: res.categoryTitle[lang] });  //设置当前页面的title
         });
 
+        this.setData({ levelId: options.id ? options.id : '' });
+        this.getlistInfoFn(1);
+
         //判断是否有缓存，有直接加载缓存不调用接口
-        var serveData = wx.getStorageSync('serveCategory') ? wx.getStorageSync('serveCategory'):null;
-        if (!serveData){
-            this.setData({ levelId: options.id ? options.id:'' });
-            this.getlistInfoFn(1);
-        }else{
-            var levelId = options.id ? options.id: serveData.level1[0].id
-            this.setData({
-                serveData : serveData,
-                levelId : levelId
-            })
-        }
+        // var serveData = wx.getStorageSync('serveCategory') ? wx.getStorageSync('serveCategory'):null;
+        // if (!serveData){
+        //     this.setData({ levelId: options.id ? options.id:'' });
+        //     this.getlistInfoFn(1);
+        // }else{
+        //     var levelId = options.id ? options.id: serveData.level1[0].id
+        //     this.setData({
+        //         serveData : serveData,
+        //         levelId : levelId
+        //     })
+        // }
     },
 
     //选项卡切换
@@ -49,7 +51,6 @@ Page({
     getlistInfoFn(level) {
         var _this = this;
         app.requestFn({
-            isLoading: level,
             url: '/serviceCategory/list',
             data:{
                 level:level
@@ -62,7 +63,7 @@ Page({
                     if (serveData.level1.length==0){ return }    //没有一级类目就不用继续加载
                     _this.getlistInfoFn(level + 1)
                 } else {
-                    wx.setStorageSync('serveCategory', _this.data.serveData) //设置缓存用户信息
+                    //wx.setStorageSync('serveCategory', _this.data.serveData) //设置缓存用户信息
                     if (!_this.data.levelId) {
                         this.setData({ levelId: serveData.level1[0].id })
                     }
